2cb03e73ee8a530f7966a575983712624b674554
Sofia Khizhnyak <pissboyyz@gmail.com>
2025-04-16 16:02:03 +0300

    bbbbgg

--- api/fatsecret-search.js
+++ api/fatsecret-search.js
@@ -6,10 +6,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).json({
-      success: true,
-      data: searchResponse.data,
-    });
+    return res.status(200).end();
   }
 
   if (req.method === "POST") {
